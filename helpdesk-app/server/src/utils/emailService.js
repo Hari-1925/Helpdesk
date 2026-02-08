4f9ba710c04fd6437e68a4ac6498ac59aa0f624d
Hari-1925 <hariqwerty72@gmail.com>
2026-02-08 14:50:46 +0530

    email confih

--- helpdesk-app/server/src/utils/emailService.js
+++ helpdesk-app/server/src/utils/emailService.js
@@ -7,7 +7,9 @@ let transporter;
 if (process.env.EMAIL_SERVICE === 'gmail') {
     // configured for Gmail
     transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 465, // SSL
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
